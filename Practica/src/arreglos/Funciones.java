ebfafdf43a99b7b1362e5327202b8b81840f323d
Guido Baldini <guidobaldini.gb@gmail.com>
2026-03-16 11:37:30 -0300

    Funciones int

--- Practica/src/arreglos/Funciones.java
+++ Practica/src/arreglos/Funciones.java
@@ -1,5 +1,7 @@
 package arreglos;
 
+import java.util.Scanner;
+
 public class Funciones {
 	public static void imprimir(int[] a) {
 		System.out.print("[ ");
@@ -7,5 +9,64 @@ public class Funciones {
 			System.out.print(a[x] + ", ");
 		}
 		System.out.println(a[a.length-1] + " ]");
+	}
+	
+	/*
+	 * Escribir la función public static int[] pedirArray(int n) que construye un array de longitud n, 
+	 * solicitando al usuario el valor de cada una de las posiciones y devuelve el array construído.
+	 */
+	
+	public static int[] pedirArray(int n) {
+		int[] arreglo = new int[n];
+		for(int x=0;x<arreglo.length;x++) {
+			arreglo[x] = pedirEntero();
+		}
+		return arreglo;
+	}
+	
+	public static int pedirEntero() {
+		Scanner lector = new Scanner(System.in);
+		System.out.println("Ingrese un entero");
+		return lector.nextInt();
+	}
+	/*
+	 * Escribir la función public static int[] rango(int m, int n) que recibe dos enteros m y n,
+	 * y devuelve un array que contiene todos los números comprendidos en el intervalo [m, n] (inclusive).
+	 * Probar la función con varios ejemplos.
+	 */
+	
+	public static int[] rango(int m, int n) {
+		int[] arreglo = new int[n-m+1];
+		for(int y=0;y<arreglo.length;y++) {
+			arreglo[y] = m + y;
+		}
+		return arreglo;
+	}
+	
+	/*
+	 * Implementar una función public static double promedio(int[] a) que dado un array de enteros,
+	 * devuelva el promedio de los elementos del array.
+	 */
+	public static double promedio(int[] a) {
+		double suma = 0;
+		for(int p=0;p<a.length;p++) {
+			suma += a[p];
+		}
+		return suma / a.length;
+	}
+	
+	/*
+	 * Implementar una función public static int max(int[] a),
+	 * que dado un array de enteros desordenado devuelva el máximo elemento del array.
+	 */
+	
+	public static int max(int[] a) {
+		int max = a[0];
+		for(int x=1; x<a.length;x++) {
+			if(a[x] > max) {
+				max = a[x];
+			}
+		}
+		return max;
 	}
 }
--- Practica/src/arreglos/Principal.java
+++ Practica/src/arreglos/Principal.java
@@ -3,7 +3,8 @@ package arreglos;
 public class Principal {
 
 	public static void main(String[] args) {
-		int[] arreglo;
+		/*
+		 * int[] arreglo;
 		arreglo = new int[5];
 		//System.out.println(arreglo[3]);
 		for(int x=0;x<arreglo.length;x++) {
@@ -12,6 +13,7 @@ public class Principal {
 		//System.out.println(arreglo[3]);
 		Funciones.imprimir(arreglo);
 		//System.out.println(arreglo);
+	
 		int[] aux = new int[arreglo.length+1];
 		for(int x=0;x<arreglo.length;x++) {
 			aux[x] = arreglo[x]; 
@@ -19,5 +21,15 @@ public class Principal {
 		Funciones.imprimir(aux);
 		aux[aux.length-1] = 10;
 		Funciones.imprimir(aux);
+		arreglo = aux;
+		Funciones.imprimir(arreglo);
+		int[] a;
+		a = Funciones.rango(10,20);
+		Funciones.imprimir(a);
+		System.out.println("El promedio es " + Funciones.promedio(a));
+		*/
+		int[] b = {1,567,123,07,212,4,7,123,567,243,56,24};
+		Funciones.imprimir(b);
+		System.out.println("El maximo es " + Funciones.max(b));
 	}
 }
